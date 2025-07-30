083a9b90e8aa5e9ee4d53450e88a25dfff0bacf0
Erick Daniel Ramirez Divas <edramirez@bi.com.gt>
2025-07-30 17:24:09 -0600

    🏗 commit inicial ejercicio 02️

--- components/Producto.cs
+++ components/Producto.cs
@@ -1,6 +1,23 @@
 ﻿namespace proyectoGrupal01.components;
 
 public class Producto
-{
-    
+{  
+    // Atributos privados
+    private static int _correlativoID = 1;
+    private int _id;
+    private string _nombreProducto;
+    private int _moneda;
+    private float _precio;
+    // Constructor
+    public Producto(string nombreProducto, int moneda, int precio)
+    {
+        if (moneda == 1 || moneda == 2)
+        {
+           
+        }
+        else
+        {
+            throw new ArgumentException("La moneda solo puede ser 1 'GTG' o 2 'USD$' "); 
+        }    
+    }
 }
